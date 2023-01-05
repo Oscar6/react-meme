0775b5579918f86e018cdeeb18d946e74b3e471c
Artemis <oscar.m1024@gmail.com>
2023-01-04 20:48:35 -0600

    fixed img path

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,9 @@ export default function Header() {
     return (
         <header className="header">
             <img 
-                alt=""
-                src="../public/images/troll-face.png" 
+                src="/images/troll-face.png" 
                 className="header--image"
+                alt=""
             />
             <h2 className="header--title">Meme Generator</h2>
             <h4 className="header--project">React Course - Project 3</h4>
